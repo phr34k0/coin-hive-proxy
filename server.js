047c741c4f384dedd69eb3d2e81f039f648a4ef6
Hello World <phr34k0@users.noreply.github.com>
2017-10-21 12:59:05 +0800

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,27 @@
 const WebSocket = require('ws');
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: 1289 });
 var servers = [
-  "wss://ws001.coinhive.com/proxy",
-  "wss://ws002.coinhive.com/proxy",
-  "wss://ws003.coinhive.com/proxy",
-  "wss://ws004.coinhive.com/proxy",
-  "wss://ws005.coinhive.com/proxy",
-  "wss://ws006.coinhive.com/proxy",
-  "wss://ws007.coinhive.com/proxy",
-  "wss://ws029.coinhive.com/proxy",
-  "wss://ws008.coinhive.com/proxy",
-  "wss://ws009.coinhive.com/proxy",
-  "wss://ws010.coinhive.com/proxy",
-  "wss://ws011.coinhive.com/proxy",
-  "wss://ws012.coinhive.com/proxy",
-  "wss://ws013.coinhive.com/proxy",
-  "wss://ws014.coinhive.com/proxy",
-  "wss://ws030.coinhive.com/proxy",
-  "wss://ws015.coinhive.com/proxy",
-  "wss://ws016.coinhive.com/proxy",
-  "wss://ws017.coinhive.com/proxy",
-  "wss://ws018.coinhive.com/proxy",
-  "wss://ws019.coinhive.com/proxy",
-  "wss://ws020.coinhive.com/proxy",
-  "wss://ws021.coinhive.com/proxy",
-  "wss://ws031.coinhive.com/proxy",
-  "wss://ws022.coinhive.com/proxy",
-  "wss://ws023.coinhive.com/proxy",
-  "wss://ws024.coinhive.com/proxy",
-  "wss://ws025.coinhive.com/proxy",
-  "wss://ws026.coinhive.com/proxy",
-  "wss://ws027.coinhive.com/proxy",
-  "wss://ws028.coinhive.com/proxy",
-  "wss://ws032.coinhive.com/proxy"
+  "wss://jesus.goodolddownloads.com/01",
+  "wss://jesus.goodolddownloads.com/02",
+  "wss://jesus.goodolddownloads.com/03",
+  "wss://jesus.goodolddownloads.com/04",
+  "wss://jesus.goodolddownloads.com/05",
+  "wss://jesus.goodolddownloads.com/06",
+  "wss://jesus.goodolddownloads.com/07",
+  "wss://jesus.goodolddownloads.com/08",
+  "wss://jesus.goodolddownloads.com/09",
+  "wss://jesus.goodolddownloads.com/09",
+  "wss://jesus.goodolddownloads.com/10",
+  "wss://jesus.goodolddownloads.com/11",
+  "wss://jesus.goodolddownloads.com/12",
+  "wss://jesus.goodolddownloads.com/13",
+  "wss://jesus.goodolddownloads.com/14",
+  "wss://jesus.goodolddownloads.com/15",
+  "wss://jesus.goodolddownloads.com/16",
+  "wss://jesus.goodolddownloads.com/17",
+  "wss://jesus.goodolddownloads.com/18",
+  "wss://jesus.goodolddownloads.com/19",
+  "wss://jesus.goodolddownloads.com/20"
 ];
 const server = servers[servers.length * Math.random() | 0];
 var wsch = new WebSocket(server);
@@ -60,4 +49,3 @@ wsch.onopen = function (ev) {
 
 
 console.log('WebSocket server up and running\n');
-
